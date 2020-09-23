b54f490de58fda2d5fe912d77291b70aceaf1cd7
Oliver Johnson <oa_johnson@outlook.com>
2020-09-23 13:38:58 -0500

    added previous session

--- main.py
+++ main.py
@@ -9,16 +9,45 @@ import shutil
 
 
 if __name__ == "__main__":
-    # Prompt for input and output folder
-    input('Hit enter to select input and output folder.')
-    input_path = filedialog.askdirectory(title='Select Input Folder')
-    output_path = filedialog.askdirectory(title='Select Output Folder')
+    input_path = ''
+    output_path = ''
 
-    # If directories are the same, ask user for different directories
-    while input_path == output_path:
-        input('Directories cannot be the same. Hit enter to select input and output folder.')
+    # Check for previous input/output
+    use_paths_found = False
+    if os.path.exists('input_dir') and os.path.exists('output_dir'):
+        in_f = open('input_dir', 'r')
+        out_f = open('output_dir', 'r')
+        prev_input = in_f.read()
+        prev_output = out_f.read()
+        if os.path.exists(prev_input) and os.path.exists(prev_output):
+            user_prev = input('Previous directories found.\nInput{}\nOutput{}\nUse these directories? (n/Y)'.format(prev_input, prev_output))
+            use_paths_found = False if user_prev == 'n' else True
+            if use_paths_found:
+                input_path = prev_input
+                output_path = prev_output
+        in_f.close()
+        out_f.close()
+
+    # Prompt for input and output folder if needed
+    if not use_paths_found:
+        input('Hit enter to select input and output folder.')
         input_path = filedialog.askdirectory(title='Select Input Folder')
         output_path = filedialog.askdirectory(title='Select Output Folder')
+
+        # If directories are the same, ask user for different directories
+        while input_path == output_path:
+            input('Directories cannot be the same. Hit enter to select input and output folder.')
+            input_path = filedialog.askdirectory(title='Select Input Folder')
+            output_path = filedialog.askdirectory(title='Select Output Folder')
+        
+        # Create previous session
+        new_prev_input = open('input_dir', 'w')
+        new_prev_input.write(input_path)
+        new_prev_input.close()
+
+        new_prev_output = open('output_dir', 'w')
+        new_prev_output.write(output_path)
+        new_prev_output.close()
 
     # Name for invalid path (might not be created if not needed)
     invalid_path = os.path.join(output_path, 'invalid')
